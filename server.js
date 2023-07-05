cfbddd05dd5f9eda848c458181e898f1179dfd2f
BABA HUSSAIN SHAIK <111945183+bhsk85@users.noreply.github.com>
2023-07-05 13:51:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.use('/user', require('./user/index'));
+app.use('/contests', require('./contests/index'));
+
+app.listen(80,() => {
+    console.log('Example app listening...!');
+});
+
+// Export the Express API
+module.exports = app;
